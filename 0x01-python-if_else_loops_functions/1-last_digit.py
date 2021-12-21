e9ddf0e64529c8ac980b7a44df7f33965e57f229
elkinguerrero007 <3439@holbertonschool.com>
2021-12-21 20:41:49 +0000

    higher_level_programming

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,6 @@ last_digit = (abs(number) % 10)
 if number < 0:
     last_digit = last_digit * -1
 if number < 0:
-    last_digit = last_digit * -1
 if last_digit == 0:
     msg = "and is 0"
 elif last_digit > 5:

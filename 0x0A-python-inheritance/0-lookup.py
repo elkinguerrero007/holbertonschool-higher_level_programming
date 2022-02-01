f03f83787b29a62901f0ddfbafb72bd325c8b008
elkinguerrero007 <3439@holbertonschool.com>
2022-02-01 00:55:34 +0000

    higher_level_programming

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+lookup:
+    Args:
+    obj: Object
+"""
+
+
 def lookup(obj):
-    """Return list of obj"""
+    """lookup
+        Args:
+            obj: object
+    """
     return dir(obj)

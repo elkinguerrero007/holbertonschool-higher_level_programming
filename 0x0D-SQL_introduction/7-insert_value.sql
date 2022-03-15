d0da015b3cab104a53a51d8986d23590661c47c6
elkinguerrero007 <3439@holbertonschool.com>
2022-03-15 15:44:23 +0000

    sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- insert value
-INSERT INTO first_table (id, name) VALUES(89, "Holberton School");
-
+-- Inserts a new row in the table first_table.
+-- Syntax: INSERT [LOW_PRIORITY | DELAYED | HIGH_PRIORITY] [IGNORE][INTO] tbl_name
+INSERT INTO first_table (id, name) VALUES(89, 'Best School');

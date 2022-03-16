fabccdf92c964d2a09d90a23ae9790d34637465c
elkinguerrero007 <3439@holbertonschool.com>
2022-03-16 03:16:19 +0000

    sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- move to utf8
+use hbtn_0c_0
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

779785dd0a774d5864c2bcb3e7e0f9ea5ee25351
elkinguerrero007 <3439@holbertonschool.com>
2022-03-18 04:14:37 +0000

    0x0D-SQL_introduction

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+SELECT g.name "genre", COUNT(sg.genre_id) "number_of_shows" 
+FROM tv_genres g
+JOIN tv_show_genres sg
+     ON g.id = sg.genre_id
+GROUP BY sg.genre_id
+ORDER BY COUNT(sg.genre_id)  DESC;

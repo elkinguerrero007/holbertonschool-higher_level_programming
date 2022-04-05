834a05ef9798312f12932c841be151131216a835
elkinguerrero007 <3439@holbertonschool.com>
2022-04-05 02:12:49 +0000

    java_script

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Computes and prints the factorial of a given number.
+
+const { argv } = require('process');
+
+const number = parseInt(argv[2]);
+
+const factorial = number => {
+  if (number === 0) {
+    return (1);
+  }
+
+  return (number * factorial(number - 1));
+};
+
+if (isNaN(number)) {
+  console.log(1);
+} else {
+  console.log(factorial(number));
+}

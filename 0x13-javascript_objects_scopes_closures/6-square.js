344ec83cd6a933a1d87c2672fa9a573e891ee987
elkinguerrero007 <3439@holbertonschool.com>
2022-04-07 23:46:46 +0000

    java_script

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    this.cell = c || 'X';
+    this.print();
+  }
+}
+
+module.exports = Square;

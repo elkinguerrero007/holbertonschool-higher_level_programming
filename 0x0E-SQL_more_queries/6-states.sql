d36474a6d58b13a1fd92942456d8c12235ba77f8
Quinto <85587286+elkinguerrero007@users.noreply.github.com>
2026-03-17 00:18:01 -0500

    Add comment for database and states table creation
    Added a comment to describe the database and table creation.

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
--- creates database with tables
+-- Create database and states table for Holberton exercise
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-        id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        name VARCHAR(256) NOT NULL
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
 );
